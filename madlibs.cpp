8f330566618dcef4ad5223da0df32e2a54f45d5c
Reika H <61173448+reikahayashi@users.noreply.github.com>
2021-01-24 12:20:34 -0500

    Create madlibs.cpp

---
+++ madlibs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    
+    string color, noun, celebrity;
+    cout << "Enter a color: ";
+    getline(cin, color);
+    
+    cout << "Enter a noun: ";
+    getline(cin, noun);
+    
+    cout << "Enter a celebrity: ";
+    getline(cin, celebrity);
+    
+    
+
+    cout << "Roses are "<< color << endl;
+    cout << noun << " are blue" << endl;
+    cout << "I love " << celebrity << endl;
+    
+    
+    
+}
